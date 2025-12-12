ffdadce1feb8b62b6ca84a23e7d783c9024925c7
said boutayeb <saidmoney115@gmail.com>
2025-12-12 10:38:40 +0100

    v53

--- days_piscine/C00/ex08/ft_print_combn.c
+++ days_piscine/C00/ex08/ft_print_combn.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: said-boutayeb <sboutaye@student.1337.ma>   +#+  +:+       +#+        */
+/*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/27 12:43:41 by said-boutayeb     #+#    #+#             */
-/*   Updated: 2025/12/09 20:05:40 by Itachi-Logic     ###   ########.fr       */
+/*   Created: 2025/12/12 08:58:36 by Itachi-Logic      #+#    #+#             */
+/*   Updated: 2025/12/12 10:37:01 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//#include <stdio.h>
-//#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
@@ -20,65 +18,46 @@ void	ft_putchar(char c)
 	return ;
 }
 
-int	is_last_combination(int *array, int n)
+void	ft_generate_comb(int *comb, int n, int index, int value)
 {
 	int	i;
-	int	last_possible_digit;
 
-	last_possible_digit = 10 - n;
-	i = 0;
-	while (i < n)
-	{
-		if (array[i] != last_possible_digit + i)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-void	ft_recartion(int *array, int n, int current_index, int start_digit)
-{
-	int	digit;
-	int	i;
-
-	digit = start_digit;
-	if (current_index == n)
+	if (index == n)
 	{
 		i = 0;
 		while (i < n)
 		{
-			ft_putchar(array[i] + 48);
+			ft_putchar(comb[i] + 48);
 			i++;
 		}
-		if (!is_last_combination(array, n))
+		if (!(comb[0] == 10 - n))
 		{
 			ft_putchar(',');
 			ft_putchar(' ');
 		}
 		return ;
 	}
-	while (digit <= 9)
+	while (value < 10)
 	{
-		array[current_index] = digit;
-		ft_recartion(array, n, current_index + 1, digit + 1);
-		digit++;
+		comb[index] = value;
+		ft_generate_comb(comb, n, index + 1, value + 1);
+		value++;
 	}
+	return ;
 }
 
 void	ft_print_combn(int n)
 {
-	int	arry[10];
+	int	comb[10];
 
-	ft_recartion(arry, n, 0, 0);
+	if (0 < n && n < 10)
+		ft_generate_comb(comb, n, 0, 0);
+	return ;
 }
 /*
-int	main(int argc, char *argv[])
+int	main(void)
 {
-	if (argc == 2)
-	{
-		fprintf(stderr, "this is number of n: %s\n", argv[1]);
-		ft_print_combn(atoi(argv[1]));
-	}
-	write(1, "\n", 1);
+	ft_print_combn(2);
+	return (0);
 }
 */
