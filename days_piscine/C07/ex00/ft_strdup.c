1d8956a7a6ecb80f6e9019637c1c1704cd8afb9b
said boutayeb <saidmoney115@gmail.com>
2026-02-09 19:53:45 +0100

    V150

--- days_piscine/C07/ex00/ft_strdup.c
+++ days_piscine/C07/ex00/ft_strdup.c
@@ -6,8 +6,47 @@
 /*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 15:50:09 by Itachi-Logic      #+#    #+#             */
-/*   Updated: 2026/02/09 15:50:10 by Itachi-Logic     ###   ########.fr       */
+/*   Updated: 2026/02/09 19:45:56 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+//#include <stdio.h>
 
+static int	ft_length(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*dest;
+
+	len = ft_length(src);
+	dest = (char *) malloc((len + 1) * sizeof(char));
+	if (dest == 0)
+		return (dest);
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dest - len);
+}
+/*
+int	main(int argc, char *argv[])
+{
+	char	*p;
+
+	if (argc != 2)
+		return (1);
+	p = ft_strdup(argv[1]);
+	printf("this is call malloc: %s\n", p);
+	free(p);
+	return (0);
+}
+*/
